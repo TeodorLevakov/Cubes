87635e5db3ebfba61a926746fbfb7e47d0bd8cdf
TeodorLevakov <37295419+TeodorLevakov@users.noreply.github.com>
2023-10-11 16:08:11 +0200

    Create post accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -6,7 +6,13 @@ router.get('/create', (req, res) => {
     res.render('createAccessory');
 });
 
+router.post('/create', (req, res) => {
 
+    const access = req.body;
+    console.log(access);
+
+    res.render('/');
+});
 
 
 module.exports = router;
